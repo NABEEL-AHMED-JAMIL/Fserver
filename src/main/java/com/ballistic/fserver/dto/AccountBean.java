024b05ba0f37d6c5d841a98abc46a1319808194b
Nabeel Ahemd <nabeel.amd93@gmail.com>
2018-10-21 23:52:59 +0500

    Update AccountBean.java

--- src/main/java/com/ballistic/fserver/dto/AccountBean.java
+++ src/main/java/com/ballistic/fserver/dto/AccountBean.java
@@ -42,5 +42,20 @@ public class AccountBean {
 
     @Override
     public String toString() { return "AccountBean{" + "email='" + email + '\'' + ", password='" + password + '\'' + ", repository=" + file + '}'; }
+    
+    @Override
+    public String toString() {
+    	ObjectMapper mapper = new ObjectMapper();
+    	
+    	String jsonString = "";
+		try {
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			jsonString = mapper.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+    	return jsonString;
+    }
 
 }
